a67fc87b60f030d376a5ec6ae333f2e57825a756
Martin Rogel <martin.mimr@gmail.com>
2020-02-14 09:22:47 -0600

    Casi funcionando

--- Distribuidos/MT/MulMat.java
+++ Distribuidos/MT/MulMat.java
@@ -31,17 +31,17 @@ class MulMat
 				DataInputStream entrada = new DataInputStream(conexion.getInputStream());
 				DataOutputStream salida = new DataOutputStream(conexion.getOutputStream());
 				int nodo =  entrada.readInt();
-				System.out.println("Nodo recibido es:"+nodo);
 				//Reservar espacio para matriz A y B 
 				int espacio = (N/2) * (N);
-				ByteBuffer bf = ByteBuffer.allocate( 2 * espacio * (Double.BYTES));
+				ByteBuffer bb = ByteBuffer.allocate(2 * espacio * (Double.BYTES));
 				int iniA = 0,iniB = 0,finA = 0,finB = 0;
 				switch(nodo)
 				{
 					case 1:
 					// Enviar A1 y B2
 					iniA = 0;
-					finA = (N/2) - 1;
+					finA = N/2;
+					
 					iniB = N/2;
 					finB = N;
 					break;
@@ -49,39 +49,85 @@ class MulMat
 					// Enviar A2 y B1 
 					iniA = N/2;
 					finA = N;
+					
 					iniB = 0;
-					finB = (N/2) - 1;
+					finB = N/2;
 					break;
 					case 3:
 					// Enviar A2 y B2 
+					iniA = N/2;
+					finA = N;
+					
+					iniB = N/2;
+					finB = N;
+					break;
+				}
+				for(; iniA < finA; iniA++ )
+				{
+					for(int j = 0; j < N; j++)
+					{
+						bb.putDouble(A[iniA][j]);
+					}
+				}
+				for(; iniB < finB; iniB++ )
+				{
+					for(int j = 0; j < N; j++)
+					{
+						bb.putDouble(B[iniB][j]);
+					}
+				}
+
+				// Escribir y enviar 
+				byte[] bytes = bb.array();
+				salida.write(bytes);
+				salida.flush();
+				
+				// Recibir matriz calculada 
+				espacio = (N/2) * (N/2);
+				bytes = new byte[espacio*(Double.BYTES)];
+				entrada.read(bytes,0, espacio * (Double.BYTES)); 
+				bb = ByteBuffer.wrap(bytes);
+				//Usaremos iniA e iniB como donde iniciar en filas y columnas
+				//Usaremos finA y fibB como donde terminar en filas y columnas
+				switch(nodo)
+				{
+					case 1:
+					// Recibir cuadrante superior derecho
+					iniA = 0;
+					finA = N/2;
+					iniB = N/2;
+					finB = N;
+					break;
+					case 2:
+					// Recibir cuadrante inferior izquierdo
+					iniA = N/2;
+					finA = N;
+					iniB = 0;
+					finB = N/2;
+					break;
+					case 3:
+					// Recibir cuadrante inferior derecho
 					iniA = N/2;
 					finA = N;
 					iniB = N/2;
 					finB = N;
 					break;
 				}
-				// Poner matriz A
-				for(; iniA < finA; iniA++ )
-				{
-					for(int j = 0; j < N; j++)
+				double leido;
+				System.out.println("---------Nodo numero "+nodo+" --------");
+				for (; iniA < finA; iniA++)
+				{	
+					for(; iniB < finB; iniB++)
 					{
-						bf.putDouble(A[iniA][j]);
+						leido =  bb.getDouble();
+						System.out.println("Fila"+iniA+" y columna:"+iniB);
+						System.out.println("Leido: "+leido+" en flujo de entrada");
+						System.out.println("");
+						
+						C[iniA][iniB] = leido;
 					}
 				}
-				// Poner matriz B
-				for(; iniB < finB; iniB++ )
-				{
-					for(int j = 0; j < N; j++)
-					{
-						bf.putDouble(A[iniB][j]);
-					}
-				}
-				// Escribir y enviar 
-				byte[] bArr = bf.array();
-				salida.write(bArr);
-				salida.flush();
-				// Recibir matriz 
-				
+				System.out.println("Fin de nodo \n\n");
 				// Indicar termino 
 				synchronized(lock){  nt++; }
 				salida.close();	
@@ -133,28 +179,25 @@ class MulMat
 				Worker worker = new Worker(conexion);
 				worker.start();
 		}
-		synchronized(lock){  nt++; }	// Indicar termino de calculo 
- 		while( nt != 4 ){  Thread.sleep(1000);  } // Esperar finalizacion de todos los nodos
 		/* Calculo de cuadrante superior izquierdo */
 		// A1 * B1
-		/*
-		int limite = (N/2) - 1 ;
-		for(int i = 0; i < limite; i++ ) //Filas de A
+		for(int i = 0; i < N/2; i++ ) //Filas de A
 		{
-			for(int j = 0; j < limite; j++) //Filas de B
+			for(int j = 0; j < N/2; j++) //Filas de B
 			{
 				 double suma = 0;
 				 for(int k = 0; k < N ; k++) //Columnas
 				 {
-					suma += A[i][k] + B[j][k]; 
+					suma += A[i][k] * B[j][k]; 
 				 }
 				 C[i][j] = suma;
  			}
 		}
 		synchronized(lock){  nt++; }	// Indicar termino de calculo 
  		while( nt != 4 ){  Thread.sleep(1000);  } // Esperar finalizacion de todos los nodos
+		System.out.println("Matriz C calculada es:");
+		imprimirMatriz(C);
 		// Calcular checksum de la matriz C 
-		*/
 	 }else
      {
 		A = new double[N/2][N];
@@ -169,21 +212,57 @@ class MulMat
 			salida.flush();
 			// Recibir matrices
 			int espacio = (N/2) * N;
-            byte[] arrA = new byte[espacio*(Double.BYTES)];
-			byte[] arrB = new byte[espacio*(Double.BYTES)];
-            entrada.read(arrA,0,espacio * (Double.BYTES)); //Leer la primera matriz
-			entrada.read(arrB,espacio, espacio * (Double.BYTES)); //Leer la segunda matriz
-            ByteBuffer bA = ByteBuffer.wrap(arrA);
-			ByteBuffer bB = ByteBuffer.wrap(arrB);
+			System.out.println("El espacio reservado es:"+ espacio);
+            byte[] bytes = new byte[espacio*(Double.BYTES)];
+			//Leer la primera matriz
+            entrada.read(bytes,0, espacio * (Double.BYTES)); 
+            ByteBuffer bb = ByteBuffer.wrap(bytes);
             for (int i = 0; i < (N/2); i++)
 			{	
 				for(int j = 0; j < N; j++)
 				{
-					A[i][j] = bA.getDouble();
-					B[i][j] = bB.getDouble();
+					A[i][j] = bb.getDouble();
 				}
 			}
-			System.out.println("Nodo numero:"+nodo);
+			//Leer la segunda matriz
+			entrada.read(bytes,0, espacio * (Double.BYTES)); 
+            bb = ByteBuffer.wrap(bytes);
+			for (int i = 0; i < (N/2); i++)
+			{	
+				for(int j = 0; j < N; j++)
+				{
+					B[i][j] = bb.getDouble();
+				}
+			}
+			//Multiplicar Matrices 
+			for(int i = 0; i < N/2; i++ ) //Filas de A
+		    {
+			    for(int j = 0; j < N/2; j++) //Filas de B
+			    {
+				   double suma = 0;
+				   for(int k = 0; k < N ; k++) //Columnas
+				   {
+					 suma += A[i][k] * B[j][k]; 
+				   }
+				   C[i][j] = suma;
+ 			    }
+		    }
+			
+			// Enviar matriz calculada 
+			espacio = (N/2) * (N/2);
+			bb = ByteBuffer.allocate(espacio * (Double.BYTES));
+			for(int i = 0; i < N/2; i++ )
+			{
+				for(int j = 0; j < N/2; j++)
+				{
+					bb.putDouble(C[i][j]);
+				}
+			} 
+			bytes = bb.array();
+			salida.write(bytes);
+			salida.flush();
+			
+			/*
 			System.out.println("Matriz A recibida es:");
 			for(int i = 0; i < (N/2); i++){
 				for(int j = 0; j < N; j++){
@@ -199,7 +278,8 @@ class MulMat
 				    System.out.print(" ");
 				}
 				System.out.println("");
-			}
+			}	
+			*/
 			entrada.close();
 			salida.close();
 			conexion.close();
